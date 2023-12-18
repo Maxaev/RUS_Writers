8f9b3ef826ddff15774d64faf159e46c88bbba78
Махаев <ro.1000@mail.ru>
2023-12-18 03:56:37 +0300

    Ok

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/roman/Desktop/Russian_WRITERS
+# Install script for directory: /Users/roman/Desktop/Russias_Writers/RUS_Writers
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/roman/Desktop/Russian_WRITERS/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/roman/Desktop/Russias_Writers/RUS_Writers/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
